9352838ddd320902659c6721b6f8e0b2f9a89ecb
enderfire99 <nachoqmt@gmail.com>
2022-08-25 00:06:05 -0300

    🎉 Primer Commit con las librerías y conexión del bot

---
+++ index.js
@@ -0,0 +1,19 @@
+const { SlashCommandRoleOption } = require("discord.js");
+
+const { Client } = require("discord.js");
+
+const client = new Client({ intents: ["Guilds"] });
+
+client.config = require("./config.json");
+
+client
+  .login(client.config.token)
+  .then(() => {
+    console.log(`client looged in as ${client.user.username}`);
+    client.user.setActivity(
+      `Ahora mismo en ${client.guilds.cache.size} servidores`
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
