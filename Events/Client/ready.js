45ef8ba80123f6e0c32d9e5138ddfc8fe9482774
enderfire99 <nachoqmt@gmail.com>
2022-08-28 21:11:45 -0300

    📝 Actualizacion de EventHandler
    Gracias al video de lyxcode, ya tenemos event handlerrrr

---
+++ Events/Client/ready.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "ready",
+  once: true,
+
+  execute(client) {
+    console.log("Turty esta listo");
+
+    client.user.setActivity(
+      `Ahora mismo en ${client.guilds.cache.size} servidores`
+    );
+  },
+};
--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const { Client } = require("discord.js");
-const client = new Client({ intents: ["Guilds"] });
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection,
+} = require("discord.js");
+
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
+
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages],
+  partials: [User, Message, GuildMember, ThreadMember],
+});
+
+const { loadEvents } = require("./Handlers/eventHandler");
 
 client.config = require("./config.json");
+client.events = new Collection();
 
-client
-  .login(client.config.token)
-  .then(() => {
-    console.log(`client looged in as ${client.user.username}`);
-    client.user.setActivity(
-      `Ahora mismo en ${client.guilds.cache.size} servidores`
-    );
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+loadEvents(client);
+
+client.login(client.config.token);
